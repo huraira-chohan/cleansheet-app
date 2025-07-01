be02bb3697a4b9a4b062035d265cfc3feddf9680
huraira-chohan <abuhurairachohan2006@gmail.com>
2025-07-01 12:52:35 +0500

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ def auto_clean_column(col):
 if st.session_state.active_tab == "📊 Preview":
         st.subheader("🔎 Dataset Preview")
 
-    df_display = st.session_state.df_clean.copy()
+        df_display = st.session_state.df_clean.copy()
 
     view_opt = st.radio(
         "How much data to show?",
