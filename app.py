75746630be19008bd91dd107d25ee06dbbdf4cac
huraira-chohan <abuhurairachohan2006@gmail.com>
2025-07-01 12:55:45 +0500

    Update app.py

--- app.py
+++ app.py
@@ -119,9 +119,6 @@ def auto_clean_column(col):
 # Use your previously working code blocks and place them under:
 if st.session_state.active_tab == "📊 Preview":
         st.subheader("🔎 Dataset Preview")
-        
-    
-
 
     view_opt = st.radio(
         "How much data to show?",
